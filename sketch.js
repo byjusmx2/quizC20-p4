d6aafcd41228bbb59a0b67bbe97ffa781ffda052
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-09-25 18:17:25 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -87,7 +87,7 @@ function draw() {
   background(backgroundImg);
   textSize(20);
   fill("black")
-  text("Score: "+ score,30,50);
+  text("Puntuación: "+ score,30,50);
   
   
   if (gameState===PLAY){
@@ -119,16 +119,16 @@ function draw() {
     gameOver.visible = true;
     restart.visible = true;
     
-    //set velcity of each game object to 0
+    //establecer la velocidad de cada objeto del juego como 0
     ground.velocityX = 0;
     trex.velocityY = 0;
     obstaclesGroup.setVelocityXEach(0);
     cloudsGroup.setVelocityXEach(0);
     
-    //change the trex animation
+    //cambiar la animación del trex
     trex.changeAnimation("collided",trex_collided);
     
-    //set lifetime of the game objects so that they are never destroyed
+    //establecer lifetime de los objetos del juego para que no sean destruidos nunca
     obstaclesGroup.setLifetimeEach(-1);
     cloudsGroup.setLifetimeEach(-1);
     
@@ -143,7 +143,7 @@ function draw() {
 }
 
 function spawnClouds() {
-  //write code here to spawn the clouds
+  //escribir aquí el código para aparecer las nubes
   if (frameCount % 60 === 0) {
     var cloud = createSprite(width+20,height-300,40,10);
     cloud.y = Math.round(random(100,220));
@@ -151,14 +151,14 @@ function spawnClouds() {
     cloud.scale = 0.5;
     cloud.velocityX = -3;
     
-     //assign lifetime to the variable
+     //asignar lifetime a la variable
     cloud.lifetime = 300;
     
-    //adjust the depth
+    //ajustar la profundidad
     cloud.depth = trex.depth;
     trex.depth = trex.depth+1;
     
-    //add each cloud to the group
+    //agregar cada nube al grupo
     cloudsGroup.add(cloud);
   }
   
@@ -172,7 +172,7 @@ function spawnObstacles() {
   
     obstacle.velocityX = -(6 + 3*score/100);
     
-    //generate random obstacles
+    //generar obstáculos al azar
     var rand = Math.round(random(1,2));
     switch(rand) {
       case 1: obstacle.addImage(obstacle1);
@@ -182,12 +182,12 @@ function spawnObstacles() {
       default: break;
     }
     
-    //assign scale and lifetime to the obstacle           
+    //asignar escala y lifetime al obstáculo           
     obstacle.scale = 0.3;
     obstacle.lifetime = 300;
     obstacle.depth = trex.depth;
     trex.depth +=1;
-    //add each obstacle to the group
+    //agregar cada obstáculo al grupo
     obstaclesGroup.add(obstacle);
   }
 }
